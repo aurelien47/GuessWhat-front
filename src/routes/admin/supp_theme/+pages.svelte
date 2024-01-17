f95db25d8b79c4d41e1ea6643f03c97b1926681e
Tiomil60 <guillaume.montsainjean@oclock.school>
2024-01-17 10:23:14 +0100

    rajout admin

---
+++ src/routes/admin/supp_theme/+pages.svelte
@@ -0,0 +1,20 @@
+<main>
+    <section id="gestion">
+        <h2>Supprimer un thème</h2>
+        <form class="formulaire" action="" method="">
+            <ul>
+                <li>
+                    <label for="name">choisir le thème : </label>
+                    <select name="" id="theme_select">
+                        <option value="html">html</option>
+                        <option value="js">javascript</option>
+                    </select>
+                </li>
+            </ul>
+        </form>
+        <aside>
+           <p>Attention les questions du thème seront supprimées aussi!</p>
+        </aside>
+        <button id="playButton">Valider</button>
+    </section>
+  </main>
