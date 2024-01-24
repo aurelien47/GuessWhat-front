402598496db510e9a7d409d80b83b0e0b270dbf8
Tiomil60 <guillaume.montsainjean@oclock.school>
2024-01-24 09:33:42 +0100

    ajout theme ok

--- src/routes/admin/ajout_theme/+page.server.js
+++ src/routes/admin/ajout_theme/+page.server.js
@@ -1,16 +1,17 @@
 export const actions = {
-    default: async ({ request }) => {
-        const data = await request.forData();
-        const nametheme = data.get ('theme_name')
-
-        const reponse = await fetch ('https://guesswhat-api.onrender.com/ajout_theme', {
+    default: async ({ request,cookies }) => {
+        const data = await request.formData();
+        const name = data.get ('name')
+        const token = cookies.get("token");
+        const reponse = await fetch ('https://guesswhat-api.onrender.com/admin/theme', {
            method: "POST",
            headers: {
             "Content-Type": "application/json",
             // 'Content-Type': 'application/x-www-form-urlencoded',
+            "Authorization": "Bearer " + token
           },
            body:JSON.stringify({ 
-            nametheme,
+            name,
             })
            
         });
