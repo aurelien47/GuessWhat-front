37c22f8bc5839179dd8683fed15a26389fcfd0a2
ibourkabdellah <141337646+Ibourk77@users.noreply.github.com>
2024-01-26 17:20:39 +0100

    game-vis-act

--- src/routes/game/visiteur/+page.svelte
+++ src/routes/game/visiteur/+page.svelte
@@ -1,101 +1,78 @@
 <script>
-    let selectedTheme = 'default'; 
-    const themes = ['Thème 1', 'Thème 2', 'Thème 3']; 
-    let hint = ''; // L'indice pour la question
-    let answers = [
-      { text: 'Texte de la réponse 1', isCorrect: false },
-      { text: 'Texte de la réponse 2', isCorrect: false },
-      { text: 'Texte de la réponse 3', isCorrect: true },  // Supposons que c'est la bonne réponse
-      { text: 'Texte de la réponse 4', isCorrect: false },
-      { text: 'Texte de la réponse 5', isCorrect: false },
-    ]; 
-  
-    let selectedAnswer = null; 
-  
-  
-    function handleThemeChange(event) {
-      selectedTheme = event.target.value;
-      
+  export let data;
+
+  import { onMount } from 'svelte';
+  let selectedTheme = 'default';
+  let themes = data.themes; 
+  let questions = [];
+
+  async function handleThemeChange(event) {
+    selectedTheme = event.target.value;
+    const response = await fetch(`https://guesswhat-api.onrender.com/theme/${selectedTheme}`);
+    if (response.ok) {
+      const data = await response.json();
+      questions = data.riddles;
     }
-  
-    function handleAnswerClick(answer) {
-      selectedAnswer = answer;
-      console.log(`Réponse sélectionnée: ${answer.text}`);
-    }
-  
-    function handleSubmit() {
-      if (selectedAnswer) {
-        if (selectedAnswer.isCorrect) {
-          console.log(`Bonne réponse: ${selectedAnswer.text}`);
-         
-        } else {
-          console.log(`Mauvaise réponse: ${selectedAnswer.text}`);
-          
-        }
-      } else {
-        console.log('Aucune réponse sélectionnée.');
-        
-      }
-    }
-  </script>
-  
-  <main>
-    <div class="top-section">
-      <div class="theme-section">
-        <label for="theme-select">Choix du thème :</label>
-        <select id="theme-select" bind:value={selectedTheme} on:change={handleThemeChange}>
-          {#each themes as theme}
-            <option value={theme}>{theme}</option>
-          {/each}
-        </select>
-      </div>
-      <div class="question-section">
-        <span>Question :</span>
-        <div class="question-box">
-        <!--   La question sera injectée ici -->
-        </div>
-      </div>
-     
+  }
+
+  async function checkAnswer(answer, questionId) {
+    const btnAnswers = document.querySelectorAll(`#answers-${questionId} button`);
+
+    btnAnswers.forEach((btnAnswer) => {
+      btnAnswer.setAttribute('disabled', true);
+    });
+  }
+</script>
+<main>
+  <div class="top-section">
+    <div class="theme-section">
+      <label for="theme-select">Choix du thème :</label>
+      <select id="theme-select" bind:value={selectedTheme} on:change={handleThemeChange}>
+        {#each themes as theme , i}
+          <option value="{theme.id}">{theme.name}</option>
+        {/each}
+      </select>
     </div>
-    
-    <div class="answers-section">
-      {#each answers as answer}
-        <button
-          class:selected={answer === selectedAnswer}
-          on:click={() => handleAnswerClick(answer)}
-        >
-          {answer.text}
-        </button>
+  </div>
+  {#each questions as question}
+    <div class="question-section">
+      <span>Question :</span>
+      <div class="question-box">
+        {question.content}
+      </div>
+    </div>
+    <div id={`answers-${question.id}`} class="answers-section">
+      {#each question.answers as answer}
+        <button on:click={() => checkAnswer(answer, question.id)}>{answer.content}</button>
       {/each}
     </div>
-    <div class="validation-section">
-      <button on:click={handleSubmit} type="button">VALIDER</button>
-    </div>
-  </main>
-  
-  <style>
-  
-  
-   
-    .answers-section button {
-      padding: 10px;
-      margin: 5px;
-      background-color: #677A91;
-      color: #FFCD29;
-      border: none;
-      cursor: pointer;
-      
-    }
-  
-    
-    .answers-section button.selected {
-      background-color: #FFCD29;
-      color: #2C3E50;
-    }
-  
-   
-    .answers-section button:hover {
-      opacity: 0.8; 
-    }
-  </style>
-  
+  {/each}
+</main>
+<style>
+  .top-section, .answers-section {
+    margin-bottom: 20px;
+  }
+
+  .theme-section, .question-section {
+    margin-bottom: 10px;
+  }
+
+  .question-box {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin-top: 5px;
+  }
+
+  .answers-section button {
+    padding: 10px;
+    margin: 5px;
+    background-color: #677A91;
+    color: #FFCD29;
+    border: none;
+    cursor: pointer;
+  }
+
+  .answers-section button:hover {
+    opacity: 0.8;
+  }
+</style>
