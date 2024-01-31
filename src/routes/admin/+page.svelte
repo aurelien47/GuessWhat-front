cd25dce710d055233bb078d0e22b8cbf6645d7aa
Tiomil60 <guillaume.montsainjean@oclock.school>
2024-01-31 18:01:48 +0100

    supp_devinette

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -5,7 +5,7 @@
             <li class="gestion"><a href="admin/ajout_theme/">Ajouter un thème</a></li>
             <li class="gestion"><a href="admin/supp_theme/">Supprimer un thème</a></li>
             <li class="gestion"><a href="admin/ajout_devinette">Ajouter une devinette</a></li>
-            <li class="gestion"><a href="admin/sup_devinette">Supprimer une devinette</a></li>
+            <li class="gestion"><a href="admin/supp_devinette">Supprimer une devinette</a></li>
         </ul>
       </section>
   </main>
